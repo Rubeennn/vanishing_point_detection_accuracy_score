ae14d439afe283a103322623712df60cce1a39fa
Rubeennn <ruben.ghazaryan999@gmail.com>
2024-09-25 15:19:40 +0400

    Changing the process module. The accuracy with penalty method is applied.

--- process/process.py
+++ process/process.py
@@ -1,39 +1,53 @@
-from typing import Union
-from pathlib import Path
-import json
 from utils.accuracy_score import accuracy_score
+from utils.accuracy_score_with_penalty import accuracy_score_with_penalty
 from utils.extract_vp_coords_from_json import extract_vp_coords_from_json
-from utils.generate_synthetic_vanishing_points_with_noise import (
-    generate_synthetic_vanishing_points_with_noise)
 from utils.normalize_vp_coords import normalize_vp_coords
 from utils.read_img import read_img
+from pathlib import Path
 
 
-def process(image_path: Union[str, Path],
-            vp_true_json: json):
+# image_path = '../../../../Downloads/20/image_15.jpg'
 
-    vp_true = extract_vp_coords_from_json(vp_true_json)
+def process(true_vp_json, pred_vp_json, image_path):
+    vp_true = extract_vp_coords_from_json(true_vp_json)
+    vp_pred = extract_vp_coords_from_json(pred_vp_json)
 
     img = read_img(img_path=image_path)
-
-    synthetic_prediction = generate_synthetic_vanishing_points_with_noise(vp_true)
 
     vp_true = normalize_vp_coords(vp_coordinates=vp_true,
                                   img_shape=img.shape)
 
-    synthetic_prediction = normalize_vp_coords(vp_coordinates=synthetic_prediction,
-                                               img_shape=img.shape)
+    vp_pred = normalize_vp_coords(vp_coordinates=vp_pred,
+                                  img_shape=img.shape)
 
-    overall_accuracy = accuracy_score(vanishing_point=vp_true,
-                                      predicted_point=synthetic_prediction)
+    if vp_pred.shape == vp_true.shape:
+        overall_accuracy = accuracy_score(vanishing_point=vp_true,
+                                          predicted_point=vp_pred)
+    else:
+        overall_accuracy = accuracy_score_with_penalty(vp_true_coords=vp_true,
+                                                       vp_pred_coords=vp_pred)
 
     return overall_accuracy
 
 
-# data = np.array([[-7090, 181], [350, 500]])
-# predicted_points = np.array([[-10000, 340], [340, 420]])
+num = 777
+true_json = Path(f'../vps/vp_true/vanishing_point_{num}_true.json')
+pred_json = Path(f'../vps/vp_pred/vanishing_point_{num}_predicted.json')
+image = Path(f'../../../../Downloads/vanishing_point_detection_testing_images/image_{num}.jpg')
+print(process(true_json, pred_json, image))
 
-print(process(image_path='../../../../Downloads/2023_10_24_09_46_29_954.jpg',
-              vp_true_json='../../../../Downloads/2023_10_24_09_46_29_954.json'))
-
-
+# accuracy_results = {i: 0.0 for i in [1,9,10,11,12,13,14,15,16,17,18,344,539,704,711,722,777,789,1056,1151]}
+# for index in accuracy_results.keys():
+#     try:
+#
+#         true_json = Path(f'../vps/vp_true/vanishing_point_{index}_true.json')
+#         pred_json = Path(f'../vps/vp_pred/vanishing_point_{index}_predicted.json')
+#         image = Path(f'../../../../Downloads/vanishing_point_detection_testing_images/image_{index}.jpg')
+#
+#         accuracy_results[index] = process(true_json, pred_json, image)
+#     except Exception as e:
+#         print(f'the error accured for the image number {index}: {e}')
+#
+#
+# for key, value in accuracy_results.items():
+#     print(f"{key}: {value}")
